b6d42e4c0f7fd862f781d8e245ef56c2d4ad2a19
Alex <a.ofori@rocketmail.com>
2023-05-22 22:10:36 +0100

    Final Lab Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BrewdogContainer from './containers/BrewdogContainer';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>BREWDOG BEERS</h1>
+    <BrewdogContainer/>
+    </div>
+  );
+}
+
+export default App;
